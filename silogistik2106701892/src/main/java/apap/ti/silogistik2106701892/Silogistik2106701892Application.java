e8a8b350274fd148d845c5a02c9dd3d83858924d
ivanrbnc <ivanrabbanicool@gmail.com>
2023-10-07 16:46:19 +0700

    Initial commit

---
+++ silogistik2106701892/src/main/java/apap/ti/silogistik2106701892/Silogistik2106701892Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106701892;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106701892Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106701892Application.class, args);
+	}
+
+}
